93cea633307ebcc661d0a4f94ae875f989edb47b
Bassir-Elhoussein <bassirelhoussein@gmail.com>
2024-10-03 22:19:59 +0100

    POINT-INTERNET
    android app

---
+++ app/src/main/java/com/example/younes_sabir_exam_f/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.younes_sabir_exam_f;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "younes_sabir_exam_f"
+include(":app")
+ 
